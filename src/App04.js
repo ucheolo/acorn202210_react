c7db98024e22949f832a0a0abfe14deb04baebb1
shinwoocheol <swc9704@gmail.com>
2022-12-08 11:43:03 +0900

    App04 예제 추가(onChange)

---
+++ src/App04.js
@@ -0,0 +1,45 @@
+// App04.js
+
+import { Component } from "react";
+
+class App extends Component{
+    state={
+        isRun:false
+    }
+
+    render(){
+        const myName="kim";
+        const myP=<p>p2 입니다.</p>
+        let isRun = false;
+
+        return(
+            <div className="container">
+                <h1>jsx 객체 활용하기</h1>
+                {<p>p 요소 입니다.</p>}
+                {"kim"}
+                <br />
+                {myName}
+                <br />
+                <p>p1 입니다.</p>
+                {myP}
+                <h2>아래가 달려요 인지 달리지 않아요 인지 확인!</h2>
+                { true ? <p>달려요</p> : <p>달리지 않아요</p>}
+                <h2>아래가 달려요 인지 달리지 않아요 인지 확인!</h2>
+                { isRun ? <p>달려요</p> : <p>달리지 않아요</p>}
+                <h2>state 값 활용</h2>
+                달리려면 체크 <input type="checkbox" checked={this.state.isRun} onChange={this.handleChange}/>
+                <br />
+                {this.state.isRun ? <p>달려요</p> : <p>달리지 않아요</p>}
+            </div>
+        );
+    }
+    //체크박스에 체크 상태가 변했을때 호출되는 함수
+    handleChange = (e)=>{
+        this.setState({
+            isRun:e.target.checked
+        });
+    }
+
+}
+
+export default App;
